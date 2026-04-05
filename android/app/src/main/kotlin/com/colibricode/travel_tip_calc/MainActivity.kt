898f776b83b1682886f5035e017bbc868248a40d
mike <mikepitts25@gmail.com>
2026-04-05 11:30:55 +0200

    Add Flutter platform scaffolding (ios, android, macos, web)

---
+++ android/app/src/main/kotlin/com/colibricode/travel_tip_calc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.colibricode.travel_tip_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import geolocator_apple
+import in_app_purchase_storekit
+import shared_preferences_foundation
+import sqflite_darwin
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
